211b805833227c954e770ad499c5ffa07b4390e6
toorx <pescolam@gmail.com>
2018-01-25 10:51:43 +0100

    aliases.zsh added

---
+++ zsh/aliases.zsh
@@ -0,0 +1,13 @@
+alias vi=vim
+
+alias python='python3'
+alias pip='pip3'
+
+# Filesystem aliases
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+alias ls="ls -G"
+alias grep="grep --color=auto"
