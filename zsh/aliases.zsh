c77ecdf521b0f0f3f4ede4f53a817b8e4dd220fe
toorx <pescolam@gmail.com>
2018-12-17 16:10:21 +0100

    remove pip alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,8 +2,8 @@ alias vim=/usr/local/Cellar/vim/8.0.1400_4/bin/vim
 alias vimdif=/usr/local/Cellar/vim/8.0.1400_4/bin/vimdiff
 alias vi=vim
 
-alias python='python3'
-alias pip='pip3'
+#alias python='python3'
+#alias pip='pip3'
 
 # Filesystem aliases
 alias ..='cd ..'
@@ -14,5 +14,5 @@ alias .....="cd ../../../.."
 alias ls="ls -G"
 alias grep="grep --color=auto"
 
-alias ing="/Users/pol/Dropbox/IngInf/Segon/T3"
+alias ing="/Users/pol/Dropbox/IngInf/Tercer/T2"
 alias acm="/Users/pol/Dropbox/competitive_programming/UVA"
